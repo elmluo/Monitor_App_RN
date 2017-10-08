c8db5e8d0608b93e3904307b504327bdaef9b837
luoc <luoc@kongtrolink.com>
2017-10-08 09:43:54 +0800

    植入GithubPopular 应用 完美

---
+++ index.android1.js
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import SimpleApp from './APP/page/HomePage'
+
+export default class YIYI_Scloud_App extends Component {
+  render() {
+    return (
+      <SimpleApp/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('YIYI_Scloud_App', () => YIYI_Scloud_App);
--- index.ios.js
+++ index.ios.js
@@ -4,40 +4,10 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
 } from 'react-native';
-import SimpleApp from './APP/page/HomePage'
+import setup from './js/page/setup'
 
-export default class YIYI_Scloud_App extends Component {
-  render() {
-    return (
-      <SimpleApp/>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('YIYI_Scloud_App', () => YIYI_Scloud_App);
+AppRegistry.registerComponent('imooc_gp', () => setup);
