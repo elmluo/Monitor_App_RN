51b80d0b1afb880225c68cc2fb7abd18aa702ad8
luoc <luoc@kongtrolink.com>
2017-10-30 18:36:21 +0800

    [add] 修复打包之后地图不可用问题

--- js/page/monitor/SiteDetailLocationPage.js
+++ js/page/monitor/SiteDetailLocationPage.js
@@ -7,9 +7,11 @@ import {
     Alert,
     TouchableOpacity,
     WebView,
-    Image
+    Image,
+    Platform,
 } from 'react-native';
 import NavigationBar from '../../common/NavigationBar';
+import resolveAssetSource from 'resolveAssetSource';
 
 export default class LocationPage extends React.Component {
     constructor(props) {
@@ -38,6 +40,8 @@ export default class LocationPage extends React.Component {
         )
     }
 
+
+
     render() {
         let statusBar = {
             backgroundColor: this.state.theme.themeColor,
@@ -51,19 +55,37 @@ export default class LocationPage extends React.Component {
                 style={this.state.theme.styles.navBar}
                 leftButton={this._renderLeftButton()}/>;
         let insertJSToHtml = `
-        obj={a: 120.261498, b: 30.317364};
-        var map = new AMap.Map('container',{
-            zoom: 10,
-            center: [window.obj.a,window.obj.b]
+            obj={a: 120.261498, b: 30.317364};
+            var map = new AMap.Map('container',{
+                zoom: 10,
+                center: [window.obj.a,window.obj.b]
+            });
+            map.plugin(["AMap.Scale"],function(){
+            var scale = new AMap.Scale();
+            map.addControl(scale);
         });`;
 
-        //120.261498,30.317364
+
+        // 静态html需要分别放到android的android\app\src\main\assets\locationMap.html
+        // ios 放到自己建的文件夾中。
+        let source;
+        const _source = resolveAssetSource(require('../../html/Location/locationMap.html'));
+        if (__DEV__) {
+            source = {uri: `${_source.uri}`};
+        } else {
+            const sourceAndroid = {uri: `file:///android_asset/locationMap.html`};
+            const sourceIOS = {uri: `file://${_source.uri}`};
+            source = Platform.OS === 'ios' ? sourceIOS : sourceAndroid;
+        }
+
+        alert(JSON.stringify(source));
         return (
             <View style={styles.container}>
                 {navigationBar}
                 <WebView
                     injectedJavaScript={insertJSToHtml}
-                    source={{uri: 'http://localhost:8081/js/html/Location/locationMap.html'}}
+                    // source={{uri: 'http://localhost:8081/js/html/Location/locationMap.html'}}
+                    source={source}
                     startInLoadingState={true}/>
             </View>
         )
