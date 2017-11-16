7950534d430639cda1b8dc988d69b9474edece4e
liujun <123456qwe>
2017-11-16 15:32:13 +0800

    公告推送查看后，首页数据不刷新

--- js/page/home/Home.js
+++ js/page/home/Home.js
@@ -261,6 +261,7 @@ export default class Monitor extends Component {
                 this._getFsuCount(stamp),
                 this._getWeekFsuCount(stamp),
                 this._getAlarmCount(stamp),
+                this._getNoticeNotReadCount(),
             ]).then((results) => {
                 // console.log(results);
                 // 计算告警数量总和
